bc5e6be206bc492e81cd27a1404137d5d480dfd6
shoon0224 <dltkdgns204@naver.com>
2021-03-06 17:17:26 +0900

    구현완료 (로그인 인증이 안됨)

--- Apple_Login/ViewController.swift
+++ Apple_Login/ViewController.swift
@@ -6,14 +6,60 @@
 //
 
 import UIKit
+import AuthenticationServices //애플 로그인 필수 프레임워크
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding {
+    
+    @IBOutlet var loginProviderStackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupProviderLoginView() //View에 "Sign In with Apple" 버튼 보이기
     }
+    
+    func setupProviderLoginView() {
+        let authorizationButton = ASAuthorizationAppleIDButton()
+        authorizationButton.addTarget(self, action: #selector(handleAuthorizationAppleIDButtonPress), for: .touchUpInside)
 
-
+        self.loginProviderStackView.addSubview(authorizationButton)
+        
+    } // 프레임워크에서 제공하는 "Sign In with Apple" 타입과 스타일
+    
+    @objc func handleAuthorizationAppleIDButtonPress() {
+        let appleIdRequest = ASAuthorizationAppleIDProvider().createRequest()
+        appleIdRequest.requestedScopes = [.fullName, .email]
+        
+        let controller = ASAuthorizationController(authorizationRequests: [appleIdRequest])
+        controller.delegate = self
+        controller.presentationContextProvider = self
+        controller.performRequests()
+    } // "Sign In with Apple" 버튼 클릭 시
+    
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }//Apple 로그인 화면을 모달 시트로 표시하는 함수
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        switch authorization.credential {
+        //Apple ID
+        case let appleIDCredential as ASAuthorizationAppleIDCredential:
+            let userIdentifier = appleIDCredential.user
+            let fullName = appleIDCredential.fullName
+            let email = appleIDCredential.email
+            
+            print("User ID : \(userIdentifier)")
+            print("User Email : \(email ?? "")")
+            print("User Name : \((fullName?.givenName ?? "") + (fullName?.familyName ?? "")) ")
+        default:
+            break
+        }
+    } //Apple ID 연동 성공 시
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print("로그인 실패")
+        
+    }//Apple ID 연동 실패 시
+    
 }
 
